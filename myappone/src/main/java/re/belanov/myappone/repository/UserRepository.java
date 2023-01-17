feefa69c12eb863e7d4d3da1e7fb26f949715873
Pavel Belanov <belanovpn@mail.ru>
2023-01-17 11:59:51 +0500

    add Repository and Controller

--- myappone/src/main/java/re/belanov/myappone/repository/UserRepository.java
+++ myappone/src/main/java/re/belanov/myappone/repository/UserRepository.java
@@ -1,9 +1,23 @@
 package re.belanov.myappone.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import re.belanov.myappone.model.User;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<User,Integer> {
+@Transactional(readOnly = true)
+public interface UserRepository extends JpaRepository<User, Integer> {
+    @RestResource(rel = "by-email", path = "by-email")
+    @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
+    Optional<User> findByEmailIgnoreCase(String email);
+
+
+
+
+
 }
